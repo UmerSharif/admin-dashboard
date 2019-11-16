19081eab81b178d5c01ee0df95167ff7527f7cea
umer <umer4167@hotmail.com>
2019-11-16 19:09:28 +0200

    adding basic styling to mockup

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import "./Sidenav.scss";
 export default function Sidenav() {
   return (
     <aside className="sidenav">
-      <p>side nav</p>
+      <ul className="sidenav__list">
+        <li className="sidenav__list-item">Item One</li>
+        <li className="sidenav__list-item">Item Two</li>
+        <li className="sidenav__list-item">Item Three</li>
+        <li className="sidenav__list-item">Item Four</li>
+        <li className="sidenav__list-item">Item Five</li>
+      </ul>
     </aside>
   );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,12 @@
 import React from "react";
-
+import "./Main.scss";
 export default function Main() {
   return (
     <main className="main">
-      <p>main</p>
+      <div className="main-header">
+        <div className="main-header__heading">Hello User</div>
+        <div className="main-header__updates">Recent Items</div>
+      </div>
     </main>
   );
 }
