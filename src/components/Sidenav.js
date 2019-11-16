3f0fddee9fd232cc2ec1b6b5b03d2c0401a6ca35
umer <umer4167@hotmail.com>
2019-11-16 23:12:16 +0200

    fixing sidenav colors

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -3,6 +3,9 @@ import "./Sidenav.scss";
 export default function Sidenav() {
   return (
     <aside className="sidenav">
+      <div className="logo__text">
+        <span>MaxScale Admin</span>
+      </div>
       <ul className="sidenav__list">
         <li className="sidenav__list-item"> Item One</li>
         <li className="sidenav__list-item"> Item Two</li>
