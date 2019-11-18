46828e81a1f9723cccb7d386e5f0013bbf45ebf3
umer <umer4167@hotmail.com>
2019-11-18 16:13:39 +0200

    fetching server data

---
+++ src/components/Accordian/Accordian.js
@@ -0,0 +1,45 @@
+import React, { useRef, useState, useEffect } from "react";
+import Chevron from "./Chevron";
+import "./Accordian.scss";
+export default function Accordian(props) {
+  const [active, setActive] = useState(false);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    contentRef.current.style.maxHeight = active
+      ? `${contentRef.current.scrollHeight}px`
+      : "0px";
+  }, [contentRef, active]);
+
+  const toogleActive = () => {
+    setActive(!active);
+  };
+
+  //extracting server id from children passed to accordian
+
+  // const x = props.children.props.children.map(d => {
+  //   console.log(typeof d.props.children);
+  //   return d.props.children;
+  // });
+  // console.log(x[0][1]);
+  // const serverID = x[0][1];
+
+  return (
+    <div className="accordion__section">
+      <button
+        className={`accordion ${active ? "active" : ""}`}
+        onClick={toogleActive}
+      >
+        <p className="accordion__title">View {props.serverID} </p>
+        <Chevron
+          className={active ? "accordion__icon rotate" : "accordion__icon"}
+          width={10}
+          fill={"#777"}
+        />
+      </button>
+      <div ref={contentRef} className="accordion__content">
+        {props.children}
+      </div>
+    </div>
+  );
+}
--- src/pages/ServerDetail.js
+++ src/pages/ServerDetail.js
@@ -1,35 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./ServerDetail.scss";
 export default function ServerDetail() {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://54.229.207.205:8989/v1/servers", {
+        auth: {
+          username: "user2",
+          password: "hotloaf58"
+        }
+      })
+      .then(res => {
+        console.log(res.status);
+        const { data } = res.data;
+        //console.log(serversData);
+        // const { data } = serversData;
+        // console.log(data);
+        setState(data);
+      });
+  }, []);
+  // const { data } = state;
+  console.log(state);
   return (
     <div className="server">
       <h1>server Details</h1>
-
-      <h1>tables </h1>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
-      <p>table data</p>
     </div>
   );
 }
